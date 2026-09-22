5556b5e17b78215029bf56efa405bfe82933c04f
Andrei Vasiliev <andrei-v-1984@yandex.ru>
2026-09-22 12:11:32 +0300

    Исправление замечаний

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,14 +1,53 @@
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait # Подключаем инструмент для явных ожиданий
+from selenium.webdriver.support import expected_conditions as EC # Подключаем готовые условия ожидания
+from selenium.webdriver.common.keys import Keys # Подключаем класс для отправки клавиш клавиатуры
 
 class BasePage:
     
     def __init__(self, driver):
-        self.driver = driver # Сохраняем экземпляр драйвера браузера, чтобы он был доступен во всех дочерних классах
+        self.driver = driver # Сохраняем экземпляр драйвера браузера для всех классов
 
     def find_element_with_wait(self, locator): # Метод для поиска элемента с явным ожиданием видимости
-        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)) # Ждем до 10 секунд, пока элемент появится на странице, и возвращаем его
+        return WebDriverWait(self.driver, 10).until(EC.visibility_of_element_located(locator)) # Ждем до 10 секунд, пока элемент появится на странице
 
     def click_element(self, locator): # Универсальный метод клика с ожиданием кликабельности
         element = WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable(locator)) # Ждем, пока элемент станет кликабельным
-        element.click() # Кликаем по нему
+        element.click() # Кликаем по элементу
+
+    def send_keys_to_element(self, locator, text): # Метод для ввода текста в элемент
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        element.send_keys(text) # Вводим переданное значение
+
+    def send_keys_and_press_enter(self, locator, text): # Метод для ввода текста и нажатия клавиши Enter
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        element.send_keys(text) # Вводим значение
+        element.send_keys(Keys.ENTER) # Нажимаем клавишу Enter
+
+    def get_text_from_element(self, locator): # Метод для получения текста из элемента
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        return element.text # Возвращаем текст элемента
+
+    def is_element_displayed(self, locator): # Метод для проверки отображения элемента на странице
+        return self.find_element_with_wait(locator).is_displayed() # Возвращаем True, если элемент виден
+
+    def scroll_to_element(self, locator): # Метод для прокрутки экрана к элементу
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        self.driver.execute_script("arguments[0].scrollIntoView(true);", element) # Прокручиваем страницу до элемента
+
+    def scroll_to_element_center(self, locator): # Метод для прокрутки элемента в центр экрана
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        self.driver.execute_script("arguments[0].scrollIntoView({block: 'center'});", element) # Прокручиваем к центру
+
+    def click_element_with_js(self, locator): # Метод для клика по элементу с помощью JavaScript
+        element = self.find_element_with_wait(locator) # Находим элемент с ожиданием
+        self.driver.execute_script("arguments[0].click();", element) # Кликаем по элементу через JS
+
+    def get_current_url(self): # Метод для получения текущего URL страницы
+        return self.driver.current_url # Возвращает адрес текущей страницы
+
+    def wait_for_new_window_and_switch(self): # Метод для ожидания открытия новой вкладки и переключения на нее
+        WebDriverWait(self.driver, 10).until(lambda d: len(d.window_handles) > 1) # Ждем появление второй вкладки
+        self.driver.switch_to.window(self.driver.window_handles[1]) # Переключаем фокус драйвера на новую вкладку
+
+    def wait_for_url_contains(self, url_part): # Метод для ожидания появления части URL в адресе страницы
+        return WebDriverWait(self.driver, 10).until(EC.url_contains(url_part)) # Ждем, пока в URL появится указаная часть
--- tests/test_order.py
+++ tests/test_order.py
@@ -1,44 +1,38 @@
-import pytest  # Подключаем фреймворк pytest для запуска тестов
-from selenium.webdriver.support.ui import WebDriverWait  # Подключаем класс WebDriverWait для настройки ожиданий
+import pytest  # Подключаем pytest
 from pages.main_page import MainPage  # Импортируем класс главной страницы
 from pages.order_page import OrderPage  # Импортируем класс страницы заказа
+from data import OrderData, Urls  # Импортируем тестовые данные для заказа и URL-адреса
 
-class TestOrderAndNavigation:  # Объявляем класс для группировки тестов заказа и навигации
+class TestOrderAndNavigation:  # Класс для тестов заказа и навигации по логотипам
 
-    # Создаем список с двумя разными наборами данных для параметризации
-    order_data = [
-        ("Иван", "Иванов", "Москва, ул. Арбат, 1", "Черкизовская", "+79991112233", "01.10.2026", "сутки", "Комментарий: позвонить за час"),
-        ("Анна", "Смирнова", "Москва, пр. Мира, 10", "Сокольники", "+79998887766", "02.10.2026", "двое суток", "Домофон не работает")
-    ]
-
-    # Двойная параметризация: передаем данные теста, и точку входа в сценарий заказа (верхняя или нижняя кнопка)
-    @pytest.mark.parametrize("name, last_name, address, metro, phone, date, period, comment", order_data)  # Декоратор для запуска теста с разными данными
-    @pytest.mark.parametrize("order_button", ["top", "bottom"], ids=["top_button", "bottom_button"])  # Параметризируем точку входа в сценарий заказа
-    def test_order_positive_flow(self, driver, name, last_name, address, metro, phone, date, period, comment, order_button):  # Единый тест заказа для любой точки входа
+    @pytest.mark.parametrize("name, last_name, address, metro, phone, date, period, comment", OrderData.ORDER_DATA_LIST) # Параьетризация по всем наборам данных для заказа
+    def test_order_positive_flow_top_button(self, driver, name, last_name, address, metro, phone, date, period, comment):  # Тест заказа через верхнюю кнопку
         main_page = MainPage(driver)  # Создаем объект главной страницы
-
-        if order_button == "top":  # Проверяем, какая точка входа выбрана
-            main_page.click_header_order_button()  # Кликаем по верхней кнопке "Заказать"
-        else:
-            main_page.click_footer_order_button()  # Прокручиваем страницу вниз и кликаем по нижней кнопке "Заказать"
+        main_page.click_header_order_button()  # Кликаем по верхней кнопке "Заказать"
 
         order_page = OrderPage(driver)  # Создаем объект страницы заказа
-        order_page.fill_personal_info(name, last_name, address, metro, phone)  # Заполняем форму личными данными 
-        order_page.fill_rental_info(date, period, comment)  # Заполняем данные об аренде самоката и подтверждаем заказ
-        assert order_page.check_success_popup_is_displayed()  # Проверяем, что появилось всплывающее окно об успешном создании заказа
+        order_page.fill_personal_info(name, last_name, address, metro, phone)  # Заполняем Личные данные
+        order_page.fill_rental_info(date, period, comment)  # Заполняем Данные об аренде
+        assert order_page.check_success_popup_is_displayed()  # Проверяем, что появилось окно об успешном заказе
 
-    def test_logo_scooter_redirect_to_main(self, driver):  # Тест клика по логотипу "самокат"
+    @pytest.mark.parametrize("name, last_name, address, metro, phone, date, period, comment", OrderData.ORDER_DATA_LIST) # Параьетризация по всем наборам данных для заказа
+    def test_order_positive_flow_bottom_button(self, driver, name, last_name, address, metro, phone, date, period, comment):  # Тест заказа через нижнюю кнопку
         main_page = MainPage(driver)  # Создаем объект главной страницы
-        main_page.click_header_order_button()  # Переходим на страницу заказа, чтобы оказаться не на главной
+        main_page.click_footer_order_button()  # Прокручиваем вниз и кликаем по нижней кнопке "Заказать"
 
-        main_page.click_scooter_logo()  # Кликаем по логотипу "самокат" в шапке
-        assert driver.current_url == "https://qa-scooter.praktikum-services.ru/"  # Проверяем, что текущий URL совпадает с главной страницей сервиса
+        order_page = OrderPage(driver)  # Создаем объект страницы заказа
+        order_page.fill_personal_info(name, last_name, address, metro, phone)  # Заполняем Личные данные
+        order_page.fill_rental_info(date, period, comment)  # Заполняем Данные об аренде
+        assert order_page.check_success_popup_is_displayed()  # Проверяем успешность создания заказа
+
+    def test_logo_scooter_redirect_to_main(self, driver):  # Тест клика по логотипу "Самокат"
+        main_page = MainPage(driver)  # Создаем объект главной страницы
+        main_page.click_header_order_button()  # Переходим на страницу заказа, чтобы уйти с главной
+        main_page.click_scooter_logo()  # Кликаем по логотипу "Самокат"
+        assert main_page.get_current_url() == Urls.BASE_URL  # Проверяем что вернулись на главную страницу
 
     def test_logo_yandex_redirect_to_dzen(self, driver):  # Тест клика по логотипу "Яндекс"
         main_page = MainPage(driver)  # Создаем объект главной страницы
-        main_page.click_yandex_logo()  # Кликаем по логотипу "Яндекс" в шапке сайта
-
-        WebDriverWait(driver, 10).until(lambda d: len(d.window_handles) > 1)  # Ожидаем появления второй вкладки в браузере перед переключением
-        driver.switch_to.window(driver.window_handles[1])  # Переключаем фокус драйвера на новую открывшуюся вкладку браузера
-        WebDriverWait(driver, 10).until(lambda d: "about:blank" not in d.current_url)  # Ожидаем, пока страница Дзена прогрузится и URL перестанет быть пустым (about:blank)
-        assert "dzen.ru" in driver.current_url  # Проверяем, что в открывшемся адресе новой вкладки есть домен Дзена
+        main_page.click_yandex_logo()  # Кликаем по логотипу "Яндекс"
+        main_page.wait_for_new_window_and_switch()  # Ждем открытия новой вкладки и переключаемся на нее
+        assert main_page.wait_for_url_contains(Urls.DZEN_URL)  # Проверяем что в URL новой вкладки содержится "dzen.ru"
